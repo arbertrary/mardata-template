016e39aebd5c94583b34460ce16c4ec057973289
arbertrary <arbertrary@github>
2022-01-13 14:17:12 +0100

    initial commit

---
+++ template-deck.filtered.hs
@@ -0,0 +1,11 @@
+[HorizontalRule
+,Header 1 ("slide-1",[],[]) [Str "Slide",Space,Str "1"]
+,Div ("",["box","columns"],[])
+ [Header 2 ("header-2",[],[]) [Str "header",Space,Str "2"]
+ ,Para [Str "Text"]]
+,HorizontalRule
+,Header 1 ("slide-2",[],[]) [Str "SLide",Space,Str "2"]
+,Div ("",["box","columns","example"],[])
+ [Header 2 ("header-2-1",["example"],[]) [Str "header",Space,Str "2"]
+ ,Para [Str "This",Space,Str "is",Space,Str "an",Space,Str "emphasized",Space,Str "paragraph.",Space,Str "Its",Space,Str "border",Space,Str "has",Space,Str "main",Space,Str "color",Space,Str "1"]
+ ,Para [Str "another",Space,Str "line"]]]
